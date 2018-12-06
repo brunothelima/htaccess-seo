9dfc708166aaa56dee7db07623d85b08a1c0d3eb
brunothelima <brunothelima@gmail.com>
2018-12-06 13:14:06 -0200

    First Commit

---
+++ index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>User access</title>
+</head>
+<body>
+	<h1>Page loaded via user access</h1>
+</body>
+</html>
---
+++ metas.php
@@ -0,0 +1,17 @@
+<?php
+	// REQUEST_URI avaliable in the $_GET['url'] variable
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Crawler access</title>
+	<meta name="keywords" content=""/>
+	<meta name="description" content=""/>
+	<meta property="og:title" content=""/>
+	<meta property="og:description" content=""/>
+	<meta property="og:image" content=""/>
+</head>
+<body>
+	<h1>Page loaded via crawler access</h1>
+</body>
+</html>
